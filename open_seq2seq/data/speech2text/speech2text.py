47597b0775afe3048ccd633091e22c883940cb6c
arnav1993k <arnav1993k@gmail.com>
2019-03-21 17:55:01 -0700

    writing to a file instead of printing
    Signed-off-by: arnav1993k <arnav1993k@gmail.com>

--- open_seq2seq/data/speech2text/speech2text.py
+++ open_seq2seq/data/speech2text/speech2text.py
@@ -53,6 +53,7 @@ class Speech2TextDataLayer(DataLayer):
         'num_fft': int,
         'precompute_mel_basis': bool,
         'sample_freq': int,
+        'noise_files': str,
     })
 
   def __init__(self, params, model, num_workers, worker_id):
@@ -115,6 +116,19 @@ class Speech2TextDataLayer(DataLayer):
     self._files = None
     if self.params["interactive"]:
       return
+    all_noise = []
+    if "noise_files" in params:
+      noise_files_df = pd.read_csv(params["noise_files"], encoding='utf-8')
+      noise_files = noise_files_df.loc[:,"filename"].values
+      if noise_files is not None:
+        for n in params["noise_files"]:
+          print("Adding noise file {}".format(n))
+          noise,sr = librosa.load(n,16000)
+          all_noise.append(noise)
+    if len(all_noise)>0:
+        self.all_noise=all_noise
+    else:
+        self.all_noise=None
     for csv in params['dataset_files']:
       files = pd.read_csv(csv, encoding='utf-8')
       if self._files is None:
